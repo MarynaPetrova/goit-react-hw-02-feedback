dacf195c418428aaf67a091edc571707723644da
MarynaPetrova <marina.bitkova@gmail.com>
2023-11-28 21:42:09 +0100

    po

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,13 +1,17 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import css from './feedbackOptions.module.css';
 
 class FeedbackOptions extends Component {
+  increaseFeedback = (option) => {
+    this.props.onLeaveFeedback(option);
+  }
+
   render() {
     return (
       <>
-        <button className={css.button} onClick={this.props.onLeaveFeedback}>Good</button>
-        <button className={css.button} onClick={this.props.onLeaveFeedback}>Neutral</button>
-        <button className={css.button} onClick={this.props.onLeaveFeedback}>Bad</button>
+        <button className={css.button} onClick={() => this.increaseFeedback('Good')}>Good</button>
+        <button className={css.button} onClick={() => this.increaseFeedback('Neutral')}>Neutral</button>
+        <button className={css.button} onClick={() => this.increaseFeedback('Bad')}>Bad</button>
       </>
     );
   }
